243a757db254f978aa1a4d60ed916adbddb919a6
anntrofim-debug <ann.trofim@gmail.com>
2025-11-05 11:45:40 +0100

    PageHeader + Styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Tekst og ting til footeren</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/pageHeader/PageHeader.jsx
@@ -0,0 +1,11 @@
+import styles from './pageHeader.module.css'
+
+const PageHeader = () => {
+    return (
+        <header className={styles.header}>
+            <h1>Titel</h1>
+        </header>
+    )
+}
+
+export default PageHeader;
