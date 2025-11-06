7f7ef703f01163806bb887d49222d44a0c53609d
anntrofim-debug <ann.trofim@gmail.com>
2025-11-06 11:23:03 +0100

    images

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>Tekst og ting til footeren</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/sectionText/SectionText.jsx
@@ -0,0 +1,12 @@
+import styles from "./sectionText.module.css";
+
+const SectionText = () => {
+    return (
+      <section className={styles.sectionText}>
+        <h1>Titel</h1>
+        <p>text</p>
+      </section>
+    );
+}
+
+export default SectionText;
